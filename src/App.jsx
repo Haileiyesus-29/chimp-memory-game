8b741d26165e7a80a5758ed7435c76e314e1bfcb
haileiyesus-29 <haileiyesus29@gmail.com>
2024-02-10 23:22:25 +0300

    include konva library

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Stage, Layer, Rect, Circle } from 'react-konva'
 import './App.css'
+import GameScreen from './components/GameScreen'
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   return (
+      <div className='flex justify-center w-screen h-screen p-2'>
+         <div className='w-full h-full max-w-screen-xl  rounded-md shadow-lg overflow-hidden'>
+            {/* <Home /> */}
+            <GameScreen />
+         </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   )
 }
 
 export default App
